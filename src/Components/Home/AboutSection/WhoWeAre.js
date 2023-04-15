357979d71be973641965a34ea6b10a71cadf1765
JulianaTonu <julianatonu@gmail.com>
2023-04-15 16:15:28 +0600

    done  whoweare

--- src/Components/Home/AboutSection/WhoWeAre.js
+++ src/Components/Home/AboutSection/WhoWeAre.js
@@ -1,26 +1,41 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import './WhoWeAre.css'
 const WhoWeAre = () => {
+
+const allData=[
+    {
+        id:1,
+        title:"Who we are",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit amet sit ameteuismod tincidunt ut laoreet tatum zzril delame tLorem ipsum dolor sit amet"
+    },
+    {
+        id:2,
+        title:"What we do",
+        description:"Lorem ipsum dolor sit amet ipsum dolor sit ametLorem ipsum dolor sit amet  euismod tincidunt ut laoreet tatum zzril delenit augue duis dolore  magna aliquam erat volutpat. Ut wisi  enim ad minim veniam, quis nostrud"
+    }
+]
+
     return (
-        <div className='border-2 my-10'>
-            <h3 >Who we are</h3>
-            <p className='mt-4'>Lorem ipsum dolor sit amet, consectetuer
-                adipiscing elit, sed diam nonummy nibh
-                euismod tincidunt ut laoreet dolore
-                magna aliquam erat volutpat. Ut wisi
-                enim ad minim veniam, quis nostrud
-                exerci tation ullamcorper suscipit lobortis
-                nisl ut aliquip ex ea commodo consequat.
+        
+    <div className='md:flex  mx-auto    py-36 bg-gradient-to-r from-thirdLightPrimary via-whiteSmoke to-thirdLightPrimary'>
+      <div className='w-[80%] lg:flex mx-auto'>
+        
+       {
 
-                Duis autem vel eum iriure dolor in hen-
-                drerit in vulputate velit esse molestie con-
-                sequat, vel illum dolore eu feugiat nulla
+allData.map((data, index)=>(
+    <div className='border-2 border-primary justify-center p-8 sm:w-[320px] md:w-[470px] lg:w-[500px] mb-8 rounded-3xl mx-auto' key={index}>
+    <h3 className='text-secondary font-bold text-xl'>{data.title}</h3>
+    <p className='w-[50px] border-2 border-primary'></p>
+    <p className='mt-5 text-textColor'>{data.description}</p>
+</div>
 
-                facilisis at vero eros et accumsan et iusto
+))
+}
+  
+        </div>    
+          
 
-                odio dignissim qui blandit praesent lup-
-                tatum zzril delenit augue duis</p>
-        </div>
+    </div>
     )
 }
 
