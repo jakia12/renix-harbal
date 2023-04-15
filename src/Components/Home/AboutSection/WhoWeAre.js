a7bb0c7c026acf86444a8a94422128c9f62a9ad4
JulianaTonu <julianatonu@gmail.com>
2023-04-15 14:11:03 +0600

    added whoweare

---
+++ src/Components/Home/AboutSection/WhoWeAre.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const WhoWeAre = () => {
+    return (
+        <div className='border-2 my-10'>
+            <h3 >Who we are</h3>
+            <p className='mt-4'>Lorem ipsum dolor sit amet, consectetuer
+                adipiscing elit, sed diam nonummy nibh
+                euismod tincidunt ut laoreet dolore
+                magna aliquam erat volutpat. Ut wisi
+                enim ad minim veniam, quis nostrud
+                exerci tation ullamcorper suscipit lobortis
+                nisl ut aliquip ex ea commodo consequat.
+
+                Duis autem vel eum iriure dolor in hen-
+                drerit in vulputate velit esse molestie con-
+                sequat, vel illum dolore eu feugiat nulla
+
+                facilisis at vero eros et accumsan et iusto
+
+                odio dignissim qui blandit praesent lup-
+                tatum zzril delenit augue duis</p>
+        </div>
+    )
+}
+
+export default WhoWeAre
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NaturalSolution from '../Components/Home/NaturalSolution/NaturalSolution';
+import WhoWeAre from '../Components/Home/AboutSection/WhoWeAre';
 
 const Home = () => {
     return (
         <div>
             <NaturalSolution/>
+            <WhoWeAre/>
         </div>
     );
 };
