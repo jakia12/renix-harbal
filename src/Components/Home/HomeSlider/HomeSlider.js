ccd191280e2ffb0de25b5e473e0cce65940a2a45
unknown <mayedakonika@gmail.com>
2023-04-16 13:29:53 +0600

    all device responsive fixed

--- src/Components/Home/HomeSlider/HomeSlider.js
+++ src/Components/Home/HomeSlider/HomeSlider.js
@@ -30,58 +30,46 @@ const HomeSlider = () => {
         className="mySwiper "
       >
         <SwiperSlide>
-          <div className=" bg-gradient-to-r from-white via-secondLightPrimary to-white">
-            {/* <div className="relative">
-              <img src={HeroMain} alt="" />
-            </div> */}
-
-            <div className="lg:h-screen container mx-auto flex flex-col-reverse md:flex md:flex-row md:justify-between md:items-center">
-              <div className="w-full text-center md:text-start pb-20 lg:p-16 lg:w-1/2 md:p-20">
-                <span className="uppercase text-xl  md:text-2xl font-bold text-primary">
-                  renix{" "}
-                </span>
-                <span className="uppercase text-xl md:text-2xl  font-bold ">
-                  unani
-                </span>
-                <h1 className="uppercase text-xl my-3 md:text-2xl  font-bold">
+          <div className=" bg-gradient-to-r relative from-white via-secondLightPrimary to-white">
+            <div className="lg:h-screen container mx-auto flex flex-col-reverse md:px-20 md:py-10 md:flex-row md:justify-between md:items-center">
+              <div className="w-full text-center text-xl md:text-2xl lg:text-5xl font-bold md:text-start pb-20 md:pb-0 md:pl-6 md:w-2/4 ">
+                <span className="uppercase  text-primary">renix </span>
+                <span className="uppercase  ">unani</span>
+                <h1 className="uppercase md:leading-3  lg:my-6 ">
                   laboratories limited
                 </h1>
                 <p className=" tracking-normal text-sm lg:tracking-widest font-medium md:text-xl ">
                   Healthy Living Natural Solutions
                 </p>
-                <button className="bg-primary p-2 text-sm mt-6 lg:px-6 lg:py-2 lg:my-8 md:my-6 md:px-4 md:py-2  lg:font-medium text-white">
+                <button className="bg-primary p-2 text-sm lg:text-xl mt-6 lg:px-4 lg:py-2  md:mt-6 md:px-4 md:py-2  lg:font-medium text-white">
                   Shop Now
                 </button>
               </div>
-              <div className="flex md:w-1/2 lg:1/2 justify-between self-center  items-center py-14">
+              <div className="flex md:w-1/2 justify-between  self-center  items-center py-14 md:py-0 ">
                 <div>
-                  <img
-                    className="w-2/3 -mb-6 md:-mb-10 ml-12"
-                    src={product1}
-                    alt=""
-                  />
+                  <img className="w-2/3 -mb-6  ml-12" src={product1} alt="" />
                 </div>
                 <div>
                   <img className="w-3/4 " src={product2} alt="" />
                 </div>
               </div>
-              <div className="lg:w-1/5 absolute bottom-0 right-0 w-1/3   md:bottom-0 md:right-0 md:w-1/3">
-                <img src={tree} alt="" />
-              </div>
-              <div>
-                <img
-                  className="absolute sm:w-1/3  sm:top-1/3 sm:right-0  sm:-left-8 sm:rotate-90  lg:right-3/4 lg:w-1/2 lg:rotate-0 lg:top-1/2"
-                  src={leavesBgPattern}
-                  alt=""
-                />
-              </div>
-              <div>
-                <img
-                  className="absolute sm:w-1/3 top-7 right-0 w-1/6"
-                  src={leavesBgPattern}
-                  alt=""
-                />
-              </div>
+            </div>
+            <div className=" absolute bottom-3 left-2 md:left-0 md:bottom-0 lg:bottom-11 lg:left-10 w-1/3 md:w-1/6 ">
+              <img src={tree} alt="" />
+            </div>
+            <div>
+              <img
+                className="absolute w-1/3 md:w-1/6 lg:w-1/12 top-0  right-0"
+                src={leavesBgPattern}
+                alt=""
+              />
+            </div>
+            <div>
+              <img
+                className="absolute w-1/3  md:w-1/6 lg:w-1/12 top-1/2 lg:top-3/4  right-0 "
+                src={leavesBgPattern}
+                alt=""
+              />
             </div>
           </div>
         </SwiperSlide>
