b10ed54344d9032e7bab39d50e88c8b2f1dfa722
JulianaTonu <julianatonu@gmail.com>
2023-04-16 10:49:00 +0600

    added newArrivals

--- src/Components/Home/NewArrivals/NewArrivals.js
+++ src/Components/Home/NewArrivals/NewArrivals.js
@@ -1,8 +1,35 @@
 import React from 'react'
+import img1 from '../../../Assets/images/Products/p1.png'
+import img2 from '../../../Assets/images/Products/p2.webp'
+import img3 from '../../../Assets/images/Products/p3.webp'
+import img4 from '../../../Assets/images/Products/p4.webp'
 
 const NewArrivals = () => {
+
+ 
   return (
-    <div>NewArrivals</div>
+    <div className=' w-full mx-auto my-10 text-center pt-24 pb-10 bg-[#f7fbf3]'>
+        <h3 className='bg-thirdLightPrimary w-36 mx-auto font '>F e a t u r e</h3>
+        <h1 className='text-secondary font-semibold text-3xl mt-3'>New Arrivals</h1>
+        <div className=' flex gap-6 flex-wrap mx-auto justify-center  mt-24'>
+      {
+        newProducts?.slice(0,4)?.map((product)=>(
+          
+                <div className='w-72' key={product._id}>
+            <div className='w-72 h-80 p-4 px-8 rounded-xl border-2 border-whiteSmoke '>
+             <h2 className='bg-primary w-10 text-xs rounded-sm ml-44 text-white mt-3'>Sale!</h2>
+                 <img src={product.image} alt='' className='w-44 mx-auto  h-52 mt-7'/>
+             </div>
+             <div>
+                 <h1 className='text-secondary text-sm mt-2'>{product.productName}</h1>
+                 <p className='text-lightPrimary text-sm mt-2'>Tk {product.price}</p>
+             </div>
+             </div>
+           
+        ))
+      }
+      </div>
+    </div>
   )
 }
 
