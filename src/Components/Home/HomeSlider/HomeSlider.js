b2d8427c143ef15203c3582f8847505349a08b06
unknown <mayedakonika@gmail.com>
2023-04-16 12:24:42 +0600

    mobile responsive fixed

--- src/Components/Home/HomeSlider/HomeSlider.js
+++ src/Components/Home/HomeSlider/HomeSlider.js
@@ -35,39 +35,49 @@ const HomeSlider = () => {
               <img src={HeroMain} alt="" />
             </div> */}
 
-            <div className="h-screen container mx-auto flex justify-between items-center">
-              <div className="p-16 w-1/2">
-                <span className="uppercase text-5xl font-bold text-primary">
+            <div className="lg:h-screen container mx-auto flex flex-col-reverse md:flex md:flex-row md:justify-between md:items-center">
+              <div className="w-full text-center md:text-start pb-20 lg:p-16 lg:w-1/2 md:p-20">
+                <span className="uppercase text-xl  md:text-2xl font-bold text-primary">
                   renix{" "}
                 </span>
-                <span className="uppercase text-5xl font-bold ">unani</span>
-                <h1 className="uppercase my-3 text-5xl font-bold">
+                <span className="uppercase text-xl md:text-2xl  font-bold ">
+                  unani
+                </span>
+                <h1 className="uppercase text-xl my-3 md:text-2xl  font-bold">
                   laboratories limited
                 </h1>
-                <p className="text-justify font-medium text-xl ">
+                <p className=" tracking-normal text-sm lg:tracking-widest font-medium md:text-xl ">
                   Healthy Living Natural Solutions
                 </p>
-                <button className="bg-primary px-6 py-2 my-8 font-medium text-white">
+                <button className="bg-primary p-2 text-sm mt-6 lg:px-6 lg:py-2 lg:my-8 md:my-6 md:px-4 md:py-2  lg:font-medium text-white">
                   Shop Now
                 </button>
               </div>
-              <div className="flex w-1/2 justify-center items-center">
+              <div className="flex md:w-1/2 lg:1/2 justify-between self-center  items-center py-14">
                 <div>
-                  <img className="w-2/3 -mb-10" src={product1} alt="" />
+                  <img
+                    className="w-2/3 -mb-6 md:-mb-10 ml-12"
+                    src={product1}
+                    alt=""
+                  />
                 </div>
                 <div>
                   <img className="w-3/4 " src={product2} alt="" />
                 </div>
               </div>
-              <div className="w-1/5 absolute top-1/2 left-0">
+              <div className="lg:w-1/5 absolute bottom-0 right-0 w-1/3   md:bottom-0 md:right-0 md:w-1/3">
                 <img src={tree} alt="" />
               </div>
               <div>
-                <img className="absolute " src={leavesBgPattern} alt="" />
+                <img
+                  className="absolute sm:w-1/3  sm:top-1/3 sm:right-0  sm:-left-8 sm:rotate-90  lg:right-3/4 lg:w-1/2 lg:rotate-0 lg:top-1/2"
+                  src={leavesBgPattern}
+                  alt=""
+                />
               </div>
               <div>
                 <img
-                  className="absolute top-7 right-0 w-1/6"
+                  className="absolute sm:w-1/3 top-7 right-0 w-1/6"
                   src={leavesBgPattern}
                   alt=""
                 />
