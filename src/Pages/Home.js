b1f2f743223028933fafff16aaf340d309d0f204
unknown <mayedakonika@gmail.com>
2023-04-17 12:54:33 +0600

    mad added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeSlider from "../Components/Home/HomeSlider/HomeSlider/HomeSlider";
 import HealthCareProducts from "../Components/Home/HealthCareProducts/HealthCareProducts/HealthCareProducts";
+import LocationMap from "../Components/Home/LocationMap/LocationMap";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       Home
       <HomeSlider></HomeSlider>
       <HealthCareProducts></HealthCareProducts>
+      <LocationMap></LocationMap>
     </div>
   );
 };
