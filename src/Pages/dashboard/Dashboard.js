5fba38b0f4eb1b73df8c93cb8e2246c48620574f
unknown <mayedakonika@gmail.com>
2023-05-03 09:06:59 +0600

    added

--- src/Pages/dashboard/Dashboard.js
+++ src/Pages/dashboard/Dashboard.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
-    return (
-        
-    )
-}
+  return <></>;
+};
 
-export default Dashboard
+export default Dashboard;
