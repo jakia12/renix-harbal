d90e7ba16902cf315ef87f1bc4ec69b73df38f81
jakia12 <owasi110@gmail.com>
2023-06-23 16:23:58 +0600

    add to cart button added

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -7,6 +7,8 @@ import img5 from "../Assets/images/sliderImages/p5.png";
 import img6 from "../Assets/images/sliderImages/p6.png";
 import { Link } from "react-router-dom";
 
+import { FiHeart } from 'react-icons/fi';
+
 const Products = () => {
   const [displayButton, setDisplayButton] = useState("");
   const products = [
@@ -97,10 +99,10 @@ const Products = () => {
 
               >
                 <div
-                  className="m-3 border border-gray-100 p-4"
+                  className="overflow-hidden product_card m-3 border border-[#ddd] rounded-lg shadow-lg shadow-[#88888878] "
                   key={product._id}
                 >
-                  <div className="">
+                  <div className="p-4 relative overflow-hidden ">
                     <div className="flex justify-between items-center ">
                       <span class="bg-red-100 text-red-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300">
                         Sold out
@@ -112,13 +114,14 @@ const Products = () => {
                     <img
                       src={product.image}
                       alt=""
-                      className="w-44 mx-auto h-52 "
+                      className="w-[161px] h-[203px] mx-auto "
                     />
-                    <p className="text-lightPrimary my-2 font-medium text-xs float-right">
-                      Stock: {product.stock}
-                    </p>
+                    {/* add to cart button */}
+                    <span className="btn_overlay opacity-0 invisible transition_move absolute bottom-[-100%] left-0 bg-primary  hover:bg-textColor p-4 text-center right-0 w-full">
+                      <button className="text-white text-base">Add To Cart</button>
+                    </span>
                   </div>
-                  <div>
+                  <div className="bg-[#f7f7f7] p-4 text-center overflow-hidden">
                     <h1 className="text-secondary text-sm mt-2">
                       {product.productName}
                     </h1>
@@ -128,22 +131,23 @@ const Products = () => {
                     <h1 className="text-secondary text-sm mt-2">
                       Strength: {product.strength}
                     </h1>
-                    <p className="text-lightPrimary text-sm mt-2">
-                      BDT {product.price}
+
+                    {/* rating icon */}
+
+                    <div class="flex items-center justify-center my-3">
+                      <svg aria-hidden="true" class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>First star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                      <svg aria-hidden="true" class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Second star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                      <svg aria-hidden="true" class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                      <svg aria-hidden="true" class="w-5 h-5 text-yellow-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                      <svg aria-hidden="true" class="w-5 h-5 text-[#cdc9c9] dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
+                    </div>
+
+                    <p className="text-textColor text-sm mt-2">
+                      Price: BDT {product.price}
                     </p>
                   </div>
                 </div>
-                <div className="w-full">
-                  {displayButton === product?._id && (
-                    <button
-                      className="bg-primary 
-                       opacity-100 w-full
-                        py-2  text-center top-1/2 text-white absolute"
-                    >
-                      Add to cart
-                    </button>
-                  )}
-                </div>
+
               </div>
 
             ))}
